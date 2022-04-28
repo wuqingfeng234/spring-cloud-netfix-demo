93628e371d9805448ca25045f0da7111996f3410
wuqingfeng <wuqingfeng234@163.com>
2022-04-28 20:54:42 +0800

    first commit

---
+++ eureka-server/src/test/java/com/git/wuqf/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.wuqf.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
